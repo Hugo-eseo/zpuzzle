a048dd25913d527be39104779f4e1ab024e1f6ea
AurelieESEO <77393628+AurelieESEO@users.noreply.github.com>
2021-01-13 20:42:57 +0100

    v0.2
    fonction crop
    simplification de Photolmage :  fonction size bougée dans __init__

--- ImageAurelie.py
+++ ImageAurelie.py
@@ -6,6 +6,7 @@ Created on Wed Jan 13 14:23:40 2021
 """
 
 from PIL import Image
+import math
 
 image= Image.open("img_forest.jpg")
 ImageSize = image.size #ImageSize = (width, height) de image
@@ -15,7 +16,7 @@ ImageCrop = image.crop((0,0,60,40))
 
 print(image.format, image.size, image.mode)
 print (ImageCrop.format,ImageCrop.size,ImageCrop.mode)
-#image.show()
+image.show()
 #ImageCrop.show()
 
 print(ImageSize[0]/ImageSize[1])
@@ -23,8 +24,6 @@ print(ImageSize[0]/ImageSize[1])
 class Photolmage:
     def __init__(self,image):
         self.image = Image.open(image)
-    
-    def size(self):
         self.size = self.image.size
         self.width = self.size[0]
         self.height = self.size[1]
@@ -33,10 +32,25 @@ class Photolmage:
         print(self.size , self.width, self.height)
         
     def crop(self):
-        for i in range(10):
-            for j in range(10):
-                
+    def crop(self,TilesNumber):
+        self.TilesNumber = TilesNumber
+        tiles = list()
+#        coord = list()
+        for i in range(int(math.sqrt(TilesNumber))):
+            for j in range(int(math.sqrt(TilesNumber))):
+                left = i * (self.width // math.sqrt(TilesNumber))
+                top = j * self.height // math.sqrt(TilesNumber)
+                right = left + self.width // math.sqrt(TilesNumber) 
+                bottom = top + self.height // math.sqrt(TilesNumber)
+                tile = self.image.crop((left, top, right, bottom))
+                tiles.append(tile)
+#                coord.append((left, top, right, bottom))
+#        tiles[0].show()
+#        tiles[99].show()
+#        print(coord)
+return tiles
 
-image1 = Photolmage("img_forest.jpg")
-image1.size()
+image1 = Photolmage("img_forest.jpg") 
+#image1 = Photolmage("ImageMario.png")
 image1.printSize()
+image1.crop(100)
